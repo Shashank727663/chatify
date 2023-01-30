296f16b96d5968c153552975d288159ff7abc990
Shashank krishu <shashankkrishu99@gmail.com>
2023-01-30 07:38:29 +0530

    completed :)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm';
-
+import Chat from './components/messages/Chat';
+import { useState } from 'react';
 function App() {
+  const[userData,setUserData]=useState(null);
+
+  if(userData===null){
+    return (<div className="container">
+        <div className="container-title">Welcome to our Chat App</div>
+        <LoginForm
+          setUserDataForChat={setUserData}
+        />
+      </div>
+    )
+  }
   return (
     <div className="App">
-    <LoginForm/>
+    <Chat currentUserData={userData}></Chat>
   
     </div>
   );
